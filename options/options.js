dcd0f1ecf17e69b9143cb64709e00841b2db54ea
Crowdbotics <team@crowdbotics.com>
2021-08-03 12:38:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "throbbing_flower_29389Identifier",
+  url: "https://throbbing_flower_29389Identifier.botics.co",
+  api: "https://throbbing_flower_29389Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
